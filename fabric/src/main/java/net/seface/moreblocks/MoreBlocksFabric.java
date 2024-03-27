9cae22380f6cc9b3c85f41aa0d43cce1d4f9af30
Leonardo Luiz Gava <llgavamt@gmail.com>
2024-03-27 19:09:20 -0400

    add: more wiki images

--- fabric/src/main/java/net/seface/moreblocks/MoreBlocksFabric.java
+++ fabric/src/main/java/net/seface/moreblocks/MoreBlocksFabric.java
@@ -1,6 +1,7 @@
 package net.seface.moreblocks;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.Items;
 import net.seface.moreblocks.registry.*;
 
 public class MoreBlocksFabric implements ModInitializer {
