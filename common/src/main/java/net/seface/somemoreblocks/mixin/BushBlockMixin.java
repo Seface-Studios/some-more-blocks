70ddbe54ed81c909ed601608de74d3f4f801f4eb
Leonardo Luiz Gava <llgavamt@gmail.com>
2025-02-08 09:54:05 -0400

    fix: issue where sapplings and other bush-like plants don't grow

--- common/src/main/java/net/seface/somemoreblocks/mixin/BushBlockMixin.java
+++ common/src/main/java/net/seface/somemoreblocks/mixin/BushBlockMixin.java
@@ -36,13 +36,15 @@ public abstract class BushBlockMixin extends Block {
 
   @Override
   public boolean isRandomlyTicking(BlockState state) {
-    return SnowyBushRegistry.getSnowyVariation(state).isPresent() || SnowyBushRegistry.getNormalVariation(state).isPresent();
+    return
+      (SnowyBushRegistry.getSnowyVariation(state).isPresent() || SnowyBushRegistry.getNormalVariation(state).isPresent() && !super.isRandomlyTicking(state)) ||
+      (SnowyBushRegistry.getSnowyVariation(state).isEmpty() || SnowyBushRegistry.getNormalVariation(state).isEmpty() && super.isRandomlyTicking(state));
   }
 
   @Override
   public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-    this.MB$turnIntoNormalVariation(state, level, pos);
-    this.MB$turnIntoSnowVariation(state, level, pos);
+    this.SMB$turnIntoNormalVariation(state, level, pos);
+    this.SMB$turnIntoSnowVariation(state, level, pos);
   }
 
   /**
@@ -53,7 +55,7 @@ public abstract class BushBlockMixin extends Block {
    * @param pos The current block position.
    */
   @Unique
-  private void MB$turnIntoSnowVariation(BlockState state, Level level, BlockPos pos) {
+  private void SMB$turnIntoSnowVariation(BlockState state, Level level, BlockPos pos) {
     boolean isDoublePlant = state.hasProperty(DoublePlantBlock.HALF);
     boolean isSnowing = level.getBiome(pos).value().getPrecipitationAt(pos).equals(Biome.Precipitation.SNOW) && level.isRaining() && level.canSeeSky(pos);
     Optional<BlockState> snowyVariation = SnowyBushRegistry.getSnowyVariation(state);
@@ -81,7 +83,7 @@ public abstract class BushBlockMixin extends Block {
    * @param pos The current block position.
    */
   @Unique
-  private void MB$turnIntoNormalVariation(BlockState state, Level level, BlockPos pos) {
+  private void SMB$turnIntoNormalVariation(BlockState state, Level level, BlockPos pos) {
     boolean isDoublePlant = state.hasProperty(DoublePlantBlock.HALF);
     if (level.getBrightness(LightLayer.BLOCK, pos) > 11) {
       Optional<BlockState> normalVariation = SnowyBushRegistry.getNormalVariation(state);
