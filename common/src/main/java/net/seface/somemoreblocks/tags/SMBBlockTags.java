8d4f948aaedefcf0d1a06dc51624a8a9205d7a31
Leonardo Luiz Gava <llgavamt@gmail.com>
2025-05-20 23:35:12 -0400

    remove: extra block tags; fix: typo

--- common/src/main/java/net/seface/somemoreblocks/tags/SMBBlockTags.java
+++ common/src/main/java/net/seface/somemoreblocks/tags/SMBBlockTags.java
@@ -6,28 +6,22 @@ import net.minecraft.world.level.block.Block;
 import net.seface.somemoreblocks.SomeMoreBlocks;
 
 public final class SMBBlockTags {
-  public static final TagKey<Block> AZALEA_LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "azalea_leaf_litter_parent_leaves");
   public static final TagKey<Block> BIG_LILY_PAD_REPLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "big_lily_pad_replaceable");
-  public static final TagKey<Block> BIRCH_LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "birch_leaf_litter_parent_leaves");
   public static final TagKey<Block> CACTUS_PLANTS_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "cactus_plants_placeable");
   public static final TagKey<Block> CATTAIL_ON_WATER_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "cattail_on_water_placeable");
   public static final TagKey<Block> CATTAIL_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "cattail_placeable");
   public static final TagKey<Block> CONCRETE_BRICKS = SomeMoreBlocks.tagKey(Registries.BLOCK, "concrete_bricks");
   public static final TagKey<Block> CONCRETE_TILES = SomeMoreBlocks.tagKey(Registries.BLOCK, "concrete_tiles");
   public static final TagKey<Block> DUNE_GRASS_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "dune_grass_placeable");
-  public static final TagKey<Block> FLOWERING_AZALEA_LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "flowering_azalea_leaf_litter_parent_leaves");
   public static final TagKey<Block> FUNGUS_COLONIES = SomeMoreBlocks.tagKey(Registries.BLOCK, "fungus_colonies");
   public static final TagKey<Block> LEAF_LITTERS = SomeMoreBlocks.tagKey(Registries.BLOCK, "leaf_litters");
   public static final TagKey<Block> LEAF_LITTERS_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "leaf_litters_placeable");
-  public static final TagKey<Block> LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "leaf_litter_parent_leaves");
   public static final TagKey<Block> LEAF_LITTER_REPLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "leaf_litter_replaceable");
   public static final TagKey<Block> LUMINOUS_FLOWER_REPLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "luminous_flower_replaceable");
   public static final TagKey<Block> MUSHROOM_COLONIES = SomeMoreBlocks.tagKey(Registries.BLOCK, "mushroom_colonies");
   public static final TagKey<Block> MUSHROOM_COLONY_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "mushroom_colony_placeable");
   public static final TagKey<Block> NETHER_CLOVER_PLACEABLE = SomeMoreBlocks.tagKey(Registries.BLOCK, "nether_clover_placeable");
-  public static final TagKey<Block> PALE_OAK_LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "pale_oak_leaf_litter_parent_leaves");
   public static final TagKey<Block> SHINGLES = SomeMoreBlocks.tagKey(Registries.BLOCK, "shingles");
-  public static final TagKey<Block> SPRUCE_LEAF_LITTER_PARENT_LEAVES = SomeMoreBlocks.tagKey(Registries.BLOCK, "spruce_leaf_litter_parent_leaves");
   public static final TagKey<Block> TERRACOTTA_BRICKS = SomeMoreBlocks.tagKey(Registries.BLOCK, "terracotta_bricks");
   public static final TagKey<Block> TERRACOTTA_TILES = SomeMoreBlocks.tagKey(Registries.BLOCK, "terracotta_tiles");
 
