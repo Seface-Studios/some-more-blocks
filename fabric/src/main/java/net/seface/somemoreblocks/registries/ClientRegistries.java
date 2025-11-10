d846485f3a3123d9abda2bf5e4095de9a3b8c017
Leonardo Luiz Gava <llgavamt@gmail.com>
2025-11-10 14:55:51 -0400

    fix: color provider register

--- fabric/src/main/java/net/seface/somemoreblocks/registries/ClientRegistries.java
+++ fabric/src/main/java/net/seface/somemoreblocks/registries/ClientRegistries.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.item.properties.numeric.RangeSelectItemModelProperties;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.seface.somemoreblocks.Constants;
 import net.seface.somemoreblocks.item.properties.numeric.BucketVolumeProperty;
 import net.seface.somemoreblocks.item.properties.numeric.MoonPhaseProperty;
 
@@ -19,10 +20,16 @@ public class ClientRegistries {
    */
   private static void registerColorProviders() {
     ColorProviderRegistry.BLOCK.register(
-      (blockState, tint, pos, i) -> BiomeColors.getAverageFoliageColor(tint, pos), SMBBlocks.LEAF_LITTER.get());
+      (blockState, tint, pos, i) -> tint != null
+        ? BiomeColors.getAverageFoliageColor(tint, pos)
+        : Constants.LEAF_LITTER_COLOR,
+      SMBBlocks.LEAF_LITTER.get());
 
     ColorProviderRegistry.BLOCK.register(
-      (blockState, tint, pos, i) -> BiomeColors.getAverageGrassColor(tint, pos), SMBBlocks.CLOVER.get());
+      (blockState, tint, pos, i) -> tint != null
+        ? BiomeColors.getAverageGrassColor(tint, pos)
+        : Constants.CLOVER_COLOR, SMBBlocks.CLOVER.get()
+    );
   }
 
   /**
