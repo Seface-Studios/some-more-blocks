008ddad3e00d33e448d92c298d4981ce32746dd5
Leonardo Luiz Gava <llgavamt@gmail.com>
2025-11-11 15:31:50 -0400

    remove: extra statement

--- forge/src/main/java/net/seface/somemoreblocks/mixin/HoneycombItemMixin.java
+++ forge/src/main/java/net/seface/somemoreblocks/mixin/HoneycombItemMixin.java
@@ -2,7 +2,6 @@ package net.seface.somemoreblocks.mixin;
 
 import net.minecraft.world.item.HoneycombItem;
 import net.minecraft.world.level.block.state.BlockState;
-import net.seface.somemoreblocks.platform.PlatformServices;
 import net.seface.somemoreblocks.registries.SMBRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,12 +15,6 @@ public abstract class HoneycombItemMixin {
 
   @Inject(method = "getWaxed", at = @At(value = "HEAD"), cancellable = true)
   private static void getWaxedMixin(BlockState state, CallbackInfoReturnable<Optional<BlockState>> cir) {
-    /*
-     * Why we don't run this on NeoForge?
-     * Because NeoForge has a Built-in data map, so to prevent any incompatibility we need to avoid it.
-     */
-    if (PlatformServices.HELPER.is(PlatformServices.Platforms.NEOFORGE)) return;
-
     SMBRegistries.WAXED_COPPER_BLOCKS.getNext(state.getBlock())
       .ifPresent((block) -> cir.setReturnValue(Optional.of(block.withPropertiesOf(state))));
   }
